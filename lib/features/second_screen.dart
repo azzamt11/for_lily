ce3caa386d7b447a1842c3c6a8498f9ab0c9a29d
azzamt11 <dfeqhasavrymchaplctionnphysics@gmail.com>
2026-01-19 11:03:37 +0800

    fixing

--- lib/features/second_screen.dart
+++ lib/features/second_screen.dart
@@ -39,7 +39,7 @@ class _SecondScreenState extends State<SecondScreen> {
                   child: Padding(
                     padding: EdgeInsets.fromLTRB(20, 0, 20, height * 0.4),
                     child: Text(
-                      "Curhatanmu di Tumblr, Story Instagrammu yang lagi ngidam croissant 7.a.m, foto tercantikmu di Ranu Kumbolo, semuanya selalu menghiasi hari-hariku, Lily 🍁. Jadi please... cerialah lagi seperti sebelumnya. Cerialah demi orang-orang yang mencintaimu, keluargamu, sahabat-sahabatmu, teman-temanmu, dan juga penggemarmu 😊.",
+                      "Aktivitasmu yang kamu share di media sosial semuanya selalu menghiasi hari-hariku, Lily 🍁. Jadi please... cerialah lagi seperti sebelumnya. Cerialah demi orang-orang yang mencintaimu, keluargamu, sahabat-sahabatmu, teman-temanmu, dan juga penggemarmu 😊.",
                       style: textTheme.titleSmall,
                       textAlign: TextAlign.center,
                       maxLines: 10,
